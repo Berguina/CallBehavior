9d932e3256039f3769015f5f6f80f828fd9a4461
Anna Putrino <Berguina@users.noreply.github.com>
2015-05-27 15:55:13 +0200

    Create BTTask_CallBehavior.cpp

---
+++ BTTask_CallBehavior.cpp
@@ -0,0 +1,41 @@
+// Copyright A.Putrino S.Chachkov
+
+#include "CrazyRestaurant.h"
+#include "BTTask_CallBehavior.h"
+#include "MyBlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeManager.h"
+
+
+
+UBTTask_CallBehavior::UBTTask_CallBehavior(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	NodeName = "Call Behavior";
+}
+
+void UBTTask_CallBehavior::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+	for (int32 Index = 0; Index < Params.Num(); Index++)
+	{
+		Params[Index].CacheSelectedKey(GetBlackboardAsset());
+	}
+
+}
+
+EBTNodeResult::Type UBTTask_CallBehavior::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardData* OriginalBlackboard = BehaviorAsset->BlackboardAsset;
+
+
+	UMyBlackboardComponent * MyBlackboard = Cast<UMyBlackboardComponent>(OwnerComp.GetBlackboardComponent());
+	if (MyBlackboard != NULL)
+	{
+
+		MyBlackboard->PushBlackboard(OriginalBlackboard, Params);
+
+
+	}
+
+	EBTNodeResult::Type Result = Super::ExecuteTask(OwnerComp, NodeMemory);
+	return Result;
+}
