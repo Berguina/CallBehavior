973882743cc64d18f204f93cf509fd62861722bf
Anna Putrino <Berguina@users.noreply.github.com>
2015-05-27 15:57:12 +0200

    Create MyBlackboardComponent.h

---
+++ MyBlackboardComponent.h
@@ -0,0 +1,39 @@
+// Copyright A.Putrino S.Chachkov.
+
+#pragma once
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "MyBlackboardComponent.generated.h"
+
+/**
+ * 
+ */
+struct FMyKeyDelegate
+{
+	FBlackboard::FKey KeyID;
+	FDelegateHandle DelegateHandle;
+	FMyKeyDelegate(FBlackboard::FKey MyKeyID, FDelegateHandle MyDelegateHandle) :KeyID(MyKeyID), DelegateHandle(MyDelegateHandle){}
+};
+UCLASS()
+class CRAZYRESTAURANT_API UMyBlackboardComponent : public UBlackboardComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	bool PushBlackboard(UBlackboardData* NewAsset, TArray<struct FBlackboardKeySelector> &Params);
+
+
+	UFUNCTION(BlueprintCallable, Category = BehaviorTree)
+		void PopBlackboard();
+
+
+	UBlackboardData* CopyBlackboardData(UBlackboardData* OldAsset);
+
+	void CallNotifyObservers(FBlackboard::FKey KeyID);
+	
+private:
+	TArray<UBlackboardData*> BBStack;
+	TArray<struct FMyKeyDelegate> DelegateHandles;
+	TArray<int32>ParamsNumStack;
+};
