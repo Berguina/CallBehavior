3d04281965f5ceef1da16115573fa2adc3bf1d43
Stanislav Chachkov <stani@users.noreply.github.com>
2015-05-27 15:55:27 +0200

    Create MyAIController.h

---
+++ MyAIController.h
@@ -0,0 +1,26 @@
+// Copyright A.Putrino S.Chachkov
+
+#pragma once
+
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * AIController that should be used for bots which want to use Call Behavior node
+ */
+
+UCLASS()
+class CRAZYRESTAURANT_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AMyAIController(const FObjectInitializer& ObjectInitializer);
+	
+	/** Starts executing behavior tree. Need to override this to create my own bb component*/
+  virtual bool RunBehaviorTree(UBehaviorTree* BTAsset) override;
+
+	/** makes AI use specified BB asset. Create MyBBComponent */
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	bool MyUseBlackboard(UBlackboardData* BlackboardAsset);
+};
