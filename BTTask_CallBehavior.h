8afb9f5baa74a3ad42ebf725b8571b7a6bfffd54
Anna Putrino <Berguina@users.noreply.github.com>
2015-05-27 15:52:55 +0200

    Create BTTask_CallBehavior.h

---
+++ BTTask_CallBehavior.h
@@ -0,0 +1,27 @@
+// Copyright A.Putrino S.Chachkov
+
+#pragma once
+
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/Tasks/BTTask_RunBehavior.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BTTask_CallBehavior.generated.h"
+
+/**
+*
+*/
+UCLASS()
+class CRAZYRESTAURANT_API UBTTask_CallBehavior : public UBTTask_RunBehavior
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	UPROPERTY(Category = Blackboard, EditAnywhere)
+		TArray<struct FBlackboardKeySelector> Params;
+
+
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+};
